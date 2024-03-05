39f2f705739a9d2f0ad4e66f9fbd1dfd7383d83d
Tdahuyou <dahuyou_top@163.com>
2024-03-05 09:09:08 +0800

    update notes

---
+++ macos 应用菜单第一项的问题/demo2/index.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow, ipcMain, Menu } = require('electron')
+
+let win
+function createWindow() {
+  win = new BrowserWindow()
+  win.loadFile('./index.html')
+}
+
+function createMenu() {
+  const template = [
+    { label: '菜单一', submenu: [{ label: '功能一' }, { label: '功能二' }] },
+    { label: '菜单二', submenu: [{ label: '功能一' }, { label: '功能二' }] },
+  ]
+
+  // 对 macos 特殊处理
+  if (process.platform === 'darwin') {
+    template.unshift({
+      label: app.getName(),
+      submenu: [
+        { label: '功能一' },
+        { label: '功能二' },
+        {
+          label: 'Quit',
+          click() {
+            app.quit()
+          },
+        },
+        { label: '功能……' },
+      ],
+    })
+  }
+
+  const menu = Menu.buildFromTemplate(template)
+  Menu.setApplicationMenu(menu)
+}
+
+app.on('ready', () => {
+  createWindow()
+  createMenu()
+})
