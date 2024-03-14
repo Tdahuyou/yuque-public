a18ab4bebbbf12b0961a65bc1b1d267094bc4876
v_jialetao <v_jialetao@tencent.com>
2024-03-14 13:06:14 +0800

    update notes

---
+++ 【demo】使用 electron-devtools-installer 安装 vue-devtools/index.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+const {
+  default: installExtension,
+  VUEJS_DEVTOOLS,
+} = require('electron-devtools-installer')
+
+let win
+function createWindow() {
+  win = new BrowserWindow()
+  win.loadFile('./index.html')
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+  installExtension(VUEJS_DEVTOOLS)
+    .then((name) => {
+      console.log(`Added Extension:  ${name}`)
+      createWindow()
+    })
+    .catch((err) => console.log('An error occurred: ', err))
+})
